e3bb90f552be9182622d4481767722cb757399c4
TaylorFacen <taylor.facen@gmail.com>
2024-05-06 22:47:22 -0400

    Try out use action hook

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { testAction } from "./actions/test-action";
+import { useAction } from 'next-safe-action/hooks';
 
 export default function Page() {
+  const { execute } = useAction(testAction, {
+    onSuccess: ({ data }) => {
+      alert(data.random)
+    }
+  });
+
   return (
     <button onClick = { async () => {
       const response = await testAction();
-      console.log(response)
+      console.log(response);
+      execute()
     } }>Trigger Server Action</button>
   )
 }
